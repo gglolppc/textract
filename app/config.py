da0fd68fdb2d8548a51ac093d6d884f38f746346
gglolppc <gglolppc@gmail.com>
2025-09-04 13:54:43 +0300

    bug fix 3

--- app/config.py
+++ app/config.py
@@ -1,18 +1,21 @@
-from pathlib import Path
-from typing import List
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from app.paths import BASE_DIR
 
+
 class Settings(BaseSettings):
     max_upload_mb: int = 10
-    uploads_path: Path = Path("./uploads")
-    allowed_origins: List[str] = ["http://127.0.0.1:5500"]
+    uploads_path: str = "uploads"
+    allowed_origins: str = "http://127.0.0.1:5500"
     openai_api_key: str
 
     model_config = SettingsConfigDict(
         env_file=BASE_DIR / ".env",
-        env_file_encoding="utf-8",
-        env_list=["allowed_origins"],  # Add this to split comma-separated env variable
+        env_file_encoding="utf-8"
     )
+
+    @property
+    def origins_list(self) -> list[str]:
+        return [o.strip() for o in self.allowed_origins.split(",") if o.strip()]
+
 
 settings = Settings()
